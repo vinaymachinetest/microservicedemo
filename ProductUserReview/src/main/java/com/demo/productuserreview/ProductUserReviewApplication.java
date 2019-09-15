92fd329e180b846b2afb52f44f47dcd43636f821
vinay <krvinay68@gmail.com>
2019-09-16 00:10:57 +0530

    intial commit

---
+++ ProductUserReview/src/main/java/com/demo/productuserreview/ProductUserReviewApplication.java
@@ -0,0 +1,13 @@
+package com.demo.productuserreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductUserReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductUserReviewApplication.class, args);
+	}
+
+}
