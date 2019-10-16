0fc26e937a2772a7ff5c677be3fb13af7abbedb1
vinay <krvinay68@gmail.com>
2019-10-16 22:35:06 +0530

    resolve eureka descovery error

--- ProductInventory/src/main/java/com/demo/services/ProductsImpl.java
+++ ProductInventory/src/main/java/com/demo/services/ProductsImpl.java
@@ -21,7 +21,7 @@ public class ProductsImpl implements Products{
 		
 	   UserReviewList reviews = restTemplate.getForObject("http://PRODUCT-USER-REVIEW/reviewservice/" + userId, UserReviewList.class);
 	   return reviews.getUserReview().stream().map(review -> {
-				ProductInfo productInfo= restTemplate.getForObject("http://PRODUCT-INFORMATION/productinfoservice/" + review.getProductName(), ProductInfo.class);
+				ProductInfo productInfo= restTemplate.getForObject("http://PRODUCT-INFORMATION/services/productinfo/" + review.getProductName(), ProductInfo.class);
 				return new ProductDetails(productInfo.getProductId(),productInfo.getProductName(),review.getPrice());
 			 }).collect(Collectors.toList());	
 		//return null;
